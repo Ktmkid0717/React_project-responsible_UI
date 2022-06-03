67ab4bc31f510ec8b3b141a26dfbfad28cb6c7b9
Nishad <ktmkid717@gmail.com>
2022-06-04 01:16:33 +0530

    code refres

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 // import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import HomeLayout from "./HomeLayout";
-import Profile from "./profile";
 const index = () => {
   return (
     <div>
